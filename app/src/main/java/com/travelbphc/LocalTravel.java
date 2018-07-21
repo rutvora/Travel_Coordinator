a265ba244e7da520c57f9da74c5b85adb36066b2
Rut <rut.special@gmail.com>
2018-07-21 12:43:31 +0530

    Phase 1 Completed (Define destination and create data online to retrieve)

---
+++ app/src/main/java/com/travelbphc/LocalTravel.java
@@ -0,0 +1,170 @@
+package com.travelbphc;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by rutvora (www.github.com/rutvora)
+ */
+
+public class LocalTravel extends Fragment implements View.OnClickListener {
+    Place from, to;
+    TextView date, time;
+
+    public LocalTravel() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.local_travel, container, false);
+        try {
+            //FROM
+            PlaceAutocompleteFragment fromField = (PlaceAutocompleteFragment) getActivity().getFragmentManager().findFragmentById(R.id.fromField);
+            fromField.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    from = place;
+                }
+
+                @Override
+                public void onError(Status status) {
+                    Toast.makeText(getContext(), status.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //TO
+            PlaceAutocompleteFragment toField = (PlaceAutocompleteFragment) getActivity().getFragmentManager().findFragmentById(R.id.toField);
+            toField.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    to = place;
+                }
+
+                @Override
+                public void onError(Status status) {
+                    Toast.makeText(getContext(), status.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            date = rootView.findViewById(R.id.datePicker);
+            date.setOnClickListener(this);
+
+            time = rootView.findViewById(R.id.timePicker);
+            time.setOnClickListener(this);
+
+            rootView.findViewById(R.id.submit).setOnClickListener(this);
+        } catch (NullPointerException e) {
+            Toast.makeText(getContext(), "Fragment Manager not found", Toast.LENGTH_SHORT).show();
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.datePicker) {
+            int mYear, mMonth, mDay;
+            final Calendar c = Calendar.getInstance();
+            //Get current date
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            assert getContext() != null;
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        } else if (v.getId() == R.id.timePicker) {
+            int mHour, mMinute;
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            assert getContext() != null;
+            TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            time.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        } else if (v.getId() == R.id.submit) {
+            Log.w("Here", "1");
+            double fromLat, fromLong, toLat, toLong;
+            LatLng from = this.from.getLatLng();
+            LatLng to = this.to.getLatLng();
+            fromLat = Math.round((from.latitude) * 100.0) / 100.0;
+            fromLong = Math.round((from.longitude) * 100.0) / 100.0;
+            toLat = Math.round((to.latitude) * 100.0) / 100.0;
+            toLong = Math.round((to.longitude) * 100.0) / 100.0;
+
+            String collection = "(" + fromLat + "," + fromLong + ")-(" + toLat + "," + toLong + ")";
+            String time = this.time.getText().toString();
+            String[] splitTime = time.split(":");
+
+            Map<String, String> userInfo = new HashMap<>(3);
+            userInfo.put("Name", "Name1");
+            userInfo.put("Email", "Email1");
+            userInfo.put("Phone", "Phone1");
+
+            Map<String, Map<String, String>> toAdd = new HashMap<>(1);
+            toAdd.put("userID3", userInfo);
+            Log.w("Here", "2");
+            MainActivity.weakDBReference.get().collection("BITS Hyderabad").document("Local")
+                    .collection(collection).document(splitTime[0]).set(toAdd, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.w("Success", "True");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("Failed", "Error");
+                    e.printStackTrace();
+                }
+            });
+            Log.w("Here", "3");
+        } else {
+            Log.w("Unknown id", v.getId() + "");
+        }
+    }
+}
--- app/src/main/java/com/travelbphc/MainActivity.java
+++ app/src/main/java/com/travelbphc/MainActivity.java
@@ -1,9 +1,7 @@
 package com.travelbphc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,8 +11,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    static WeakReference<FirebaseFirestore> weakDBReference;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +28,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +37,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FirebaseApp.initializeApp(this);
+        db = FirebaseFirestore.getInstance();
+        weakDBReference = new WeakReference<>(db);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new LocalTravel()).commit();
     }
 
     @Override
@@ -76,21 +79,15 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_local) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_others) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
